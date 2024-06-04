32b566352aaf04cf995150e5cc84038603dd30f2
nadiavetrova <152676329+nadiavetrova@users.noreply.github.com>
2024-06-04 19:52:35 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var aboutPhoto = document.querySelector('.about-photo__box');
+
+  // Добавляем класс .show сразу при загрузке страницы
+  aboutPhoto.classList.add('show');
+});
+
+
